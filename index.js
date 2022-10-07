409b951558926e447942e06d1a7363b4e656ef47
13 Miles <bennygabel@gmail.com>
2022-10-07 13:34:37 -0400

    updates classes construction employees, engineer, intern and managers

---
+++ index.js
@@ -0,0 +1,38 @@
+const inquire = require('inquirer');
+
+const Employee = require('./lib/Employee')
+const Engineer = require('./lib/Engineer')
+const Intern   = require('./lib/Intern')
+const Manager  = require('./lib/Manager')
+
+const questions = [{
+                        type: 'input',
+                        name: 'name',
+                        message: 'Enter the manager\'s name:'
+                    },
+                   
+                    {
+                        type: 'input',
+                        name: 'id',
+                        message: 'Enter the manager\'s id:'
+                    },
+
+                    {
+                        type: 'input',
+                        name: 'email',
+                        message: 'Enter the manager\'s email:'
+                    },
+
+
+                    {
+                        type: 'list',
+                        name: 'role',
+                        message: 'Enter the persons role is:',
+                        choices:
+
+                    }
+
+
+}
+
+]
